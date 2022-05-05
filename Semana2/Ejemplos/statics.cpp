f954ec42897bc6e99e8aa2862867af14f62646d3
joebazoka <armandosuar@hotmail.com>
2022-05-05 04:59:25 -0300

    --

---
+++ Semana2/Ejemplos/statics.cpp
@@ -0,0 +1,27 @@
+
+
+
+#include <iostream>
+#include <memory>
+#include <array>
+#include "statics.h"
+
+using namespace std;
+
+namespace sts{
+
+	void fun1()
+	{
+		cout << "fun1" << endl;
+	}
+
+	void fun2()
+	{
+		cout << "fun2" << endl;
+	}
+
+	void fun3()
+	{
+		cout << "fun3" << endl;
+	}
+}
---
+++ Semana2/Ejemplos/statics.h
@@ -0,0 +1,12 @@
+#ifndef _STATH_
+#define _STATH_
+
+
+namespace sts{
+
+	void fun1();
+	void fun2();
+	void fun3();
+}
+
+#endif
